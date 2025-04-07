139f3c3766cf8fc52425b8c99fd0f8cf1417e98b
Vikas Pritam <87309275+pritam-ago@users.noreply.github.com>
2025-04-07 22:56:52 +0530

    nothing much

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/footer"
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] relative">
-      {/* Ambient background with moving particles */}
+      
       <div className="h-full w-full absolute inset-0 z-0">
         <SparklesCore
           id="tsparticlesfullpage"
