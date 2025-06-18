010ec03821e14db3b855239a9f03239d05aedd01
Vikas Pritam <87309275+pritam-ago@users.noreply.github.com>
2025-06-18 15:38:53 +0530

    resolve vercel deployment

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -61,7 +61,6 @@ export default function Home() {
         })
       }
       
-      // Clear URL parameters
       window.history.replaceState({}, document.title, window.location.pathname)
     }
   }, [searchParams])
